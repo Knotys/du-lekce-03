994040f4b3f75802d44d87b9cbde5e5bf6f9fa3b
Robert Knotek <knotyrk@seznam.cz>
2022-11-13 19:04:51 +0100

    finish my DU

--- src/com/engeto/ifloop/Main.java
+++ src/com/engeto/ifloop/Main.java
@@ -1,5 +1,7 @@
 package com.engeto.ifloop;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -21,62 +23,113 @@ public class Main {
     //endregion
 
     //region Úlohy na cykly
-    /**
+    /** Úkol 1
      * Čte čísla, která zadává uživatel a vypisuje je na výstup.
      * Metoda pokračuje ve čtení opakovaně tak dlouho, dokud uživatel zadává kladná čísla či nulu.
      * 
      * Jakmile uživatel zadá záporné číslo, metoda skončí.
      */
     public static void readIntsFromInputAndPrintItUntilNegative() {
+        int input = Support.safeReadInt();
+        while (input > 0) {
+            System.out.println("Číslo je kladné: "+input);
+            input = Support.safeReadInt();
+        }
     }
 
-    /**
+    /** Úkol 2
      * Čte čísla ze vstupu tak dlouho, dokud uživatel nezadá záporné číslo.
      * Jako návratovou hodnotu vrátí součet zadaných čísel (záporné číslo by nemělo být zahrnuto).
      * 
      * @return Součet všech zadaných čísel (záporné číslo ukončující vstup není zahrnuto).
      */
     public static int sumAllInputUntilNegative() {
-        return 0;
+        int input = Support.safeReadInt();
+        int sum = 0;
+        while (input > 0) {
+            sum += input;
+            System.out.println("Číslo je kladné: "+input);
+            System.out.println("Součet: "+sum);
+            input = Support.safeReadInt();
+        }
+        return sum;
     }
 
-    /**
+    /** Úkol 3
      * Čte čísla ze vstupu tak dlouho, dokud uživatel nezadá záporné číslo.
      * Všechna načtená čísla uloží do listu, který vrátí jako návratovou hodnotu.
      * 
      * @return List načtených hodnot (záporná hodnota není zahrnuta v listu)
      */
     public static List<Integer> storeAllInputInArrayListUntilNegative() {
-        return null;
+        List<Integer> results = new ArrayList<>();
+        int input = Support.safeReadInt();
+        while (input > 0) {
+            results.add(input);
+            input = Support.safeReadInt();
+        }
+        return results;
     }
 
-    /**
+    /** Úkol 4
      * Dostane jako parametr list čísel. Vypíše čísla na obrazovku. Každé číslo vypiš na samostatný řádek.
      * @param list List čísel, která se mají vypsat.
      */
     public static void printAllIntegersFromList(List<Integer> list) {
+        List<Integer> number = new ArrayList<>();
+        int input = Support.safeReadInt();
+        while (input > 0) {
+            number.add(input);
+            input = Support.safeReadInt();
+
+        }
+        System.out.println(number);
     }
 
-    /**
+    /** Úkol 5
      * Sečte hodnoty z listu a vrátí výsledek jako návratovou hodnotu.
      * @param list List čísel, která chceme sečíst.
      * @return Součet všech čísel v listu.
      */
     public static Integer sumAllIntegersFromList(List<Integer> list) {
-        return null;
+        List<Integer> numbers = new ArrayList<>();
+        int input = Support.safeReadInt();
+        int sum = 0;
+        while (input > 0) {
+            numbers.add(input);
+            sum += input;
+            input = Support.safeReadInt();
+        }
+        return sum;
     }
     //endregion
 
     //region Podmínky
-    /**
+    /** Úkol 6
      * Vypíše čísla ze seznamu, která jsou menší než zadaný limit.
      * @param list Seznam čísel, která máme zkontrolovat.
      * @param limit Limit - vypsána budou pouze čísla z listu, která jsou menší než limit
      */
     public static void printIntegersUnderLimit(List<Integer> list, int limit) {
+        List<Integer> listOfNumbers = new ArrayList<>();
+        int input = Support.safeReadInt();
+        limit = 5;
+        while (input > 0) {
+            listOfNumbers.add(input);
+            input = Support.safeReadInt();
+//            if (input < limit && input > 0) {
+//                System.out.println(listOfNumbers);
+//            }
+        }
+        for (int n : listOfNumbers) {
+            if (n < limit) {
+                System.out.println(n);
+            }
+        }
+
     }
 
-    /**
+    /** Úkol 7
      * Vypíše čísla ze zadaného listu.
      * Při výpisu ale nahradí:
      * - číslo 1 slovem "one", 
@@ -88,6 +141,24 @@ public class Main {
      * @param list Seznam čísel, která se mají vypsat.
      */
     public static void printIntegersWithReplace(List<Integer> list) {
+        List<String> results = new ArrayList<>();
+        int input = Support.safeReadInt();
+        String one = "one";
+        String two = "two";
+        String three = "three";
+        while (input > 0) {
+            if (input == 1) {
+                results.add(one);
+            } else if (input == 2) {
+                results.add(two);
+            } else if (input == 3) {
+                results.add(three);
+            } else {
+                results.add(String.valueOf(input));
+                input = Support.safeReadInt();
+            }
+        }
+        System.out.println(results);
     }
     //endregion
 
